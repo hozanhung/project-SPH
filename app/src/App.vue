1d325c0f5d12bc99f599cdbda8afefa7e941fbbc
hozanhung <zanhung.ho@qq.com>
2022-03-17 20:11:38 +0800

    first commit

---
+++ app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header></Header>
+    <router-view></router-view>
+    <!-- 在Home、Search显示的，在登录、注册隐藏 -->
+    <!-- <Footer v-show="$route.path=='/home'||$route.path=='/search'"></Footer> -->
+    <Footer v-show="$route.meta.show"></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+</style>
